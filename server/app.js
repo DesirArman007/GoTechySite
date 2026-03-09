04756547399ad03599876f1cf391bf3b601d0dca
Abhishek Yadav <abhishekyadav6502@gmail.com>
2026-03-09 22:13:29 +0530

    feat: add production URL to CORS configuration

--- server/app.js
+++ server/app.js
@@ -44,7 +44,8 @@ app.use(cors({
         'http://localhost:3000',
         'http://localhost:5173',
         'http://127.0.0.1:3000',
-        'http://127.0.0.1:5173'
+        'http://127.0.0.1:5173',
+        'https://go-techy-site.vercel.app/',
     ],
     credentials: true
 }));
