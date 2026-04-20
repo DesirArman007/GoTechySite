1b22dae4ca7b51d11076d651ed7879ad094dc59c
Abhishek Yadav <abhishekyadav6502@gmail.com>
2026-04-20 15:20:50 +0530

    feat: initialize server entry point with database connection and scheduled YouTube sync cron job

--- server/index.js
+++ server/index.js
@@ -1,11 +1,9 @@
+import 'dotenv/config';
 import connectDB from './config/db.js';
 import app from './app.js';
-
-import dotenv from 'dotenv';
 import { syncYouTubeVideos } from './services/youtubeSync.js';
 import cron from 'node-cron';
 
-dotenv.config();
 
 connectDB().then(() => {
     app.listen(process.env.PORT || 8080, () => {
